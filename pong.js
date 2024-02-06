d9c860fa86561d2d535389eec95d16e7d65a38a9
CosmicBearBTW <61627436+CosmicBearBTW@users.noreply.github.com>
2024-02-06 17:25:15 -0500

    Create pong.js

---
+++ pong.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById('gameCanvas');
+const context = canvas.getContext('2d');
+
+canvas.width = 800;
+canvas.height = 400;
+
+const ball = { x: canvas.width / 2, y: canvas.height / 2, radius: 10, speed: 2, dx: 2, dy: 2 };
+const paddleWidth = 75, paddleHeight = 10, paddleDiff = 25;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+function animate() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    ballMove();
+    ballDraw();
+    paddleDraw();
+    requestAnimationFrame(animate);
+}
+
+function ballMove() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    if (ball.y + ball.radius > canvas.height) {
+        ball.dy *= -1;
+    }
+    if (ball.y - ball.radius < 0) {
+        ball.dy *= -1;
+    }
+    if (ball.x + ball.radius > canvas.width) {
+        ball.dx *= -1;
+    }
+    if (ball.x - ball.radius < 0) {
+        ball.dx *= -1;
+    }
+}
+
+function ballDraw() {
+    context.beginPath();
+    context.arc(ball
