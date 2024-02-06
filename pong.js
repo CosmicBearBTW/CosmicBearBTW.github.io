b43b1f9ad3885a63bbb56e44ee42518cdcf02956
CosmicBearBTW <61627436+CosmicBearBTW@users.noreply.github.com>
2024-02-06 17:38:58 -0500

    Update pong.js

--- pong.js
+++ pong.js
@@ -4,7 +4,7 @@ const context = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 400;
 
-const ball = { x: canvas.width / 2, y: canvas.height / 2, radius: 10, speed: 2, dx: 2, dy: 2 };
+const ball = { x: canvas.width / 2, y: canvas.height - 30, radius: 10, speed: 2, dx: 2, dy: 2 };
 const paddleWidth = 75, paddleHeight = 10, paddleDiff = 25;
 let paddleX = (canvas.width - paddleWidth) / 2;
 
@@ -31,5 +31,22 @@ function ballMove() {
     if (ball.x - ball.radius < 0) {
         ball.dx *= -1;
     }
-    if (ball.x + ball.radius > paddleX && ball.y + ball.radius > canvas
-    context.arc(ball)
+    if (ball.x + ball.radius > paddleX && ball.y + ball.radius > canvas.height - paddleHeight - 5 && ball.y - ball.radius < paddleHeight + 5) {
+        ball.dy *= -1;
+    }
+}
+
+function ballDraw() {
+    context.beginPath();
+    context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2, false);
+    context.fillStyle = 'white';
+    context.fill();
+    context.closePath();
+}
+
+function paddleDraw() {
+    context.fillStyle = 'white';
+    context.fillRect(paddleX, canvas.height - paddleHeight - 5, paddleWidth, paddleHeight);
+}
+
+animate();
